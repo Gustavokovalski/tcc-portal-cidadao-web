515790125f56f5d2b0df9b418dbc79cb2afb4445
Samuel T Almeida <samuel.almeida@capgemini.com>
2021-07-04 23:32:30 -0300

    divisão de componentes

--- portal-cidadao-web/src/app/app-routing.module.ts
+++ portal-cidadao-web/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 import { LoginComponent } from './modules/auth/page/login/login.component';
 import { HomeComponent } from './modules/home/page/home.component';
 
@@ -10,16 +11,22 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: '',
+    component: ContentLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        loadChildren: () =>
+          import('./modules/home/home.module').then(m => m.HomeModule)
+      }
+    ]
+  },
+  {
     path: 'auth',
     component: LoginComponent,
     loadChildren: () =>
       import('./modules/auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-    loadChildren: () =>
-      import('./modules/home/home.module').then(m => m.HomeModule)
   }
 ];
 
--- portal-cidadao-web/src/app/app.module.ts
+++ portal-cidadao-web/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavComponent } from './layout/nav/nav.component';
 import { AuthModule } from './modules/auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    ContentLayoutComponent
   ],
   imports: [
     BrowserModule,
