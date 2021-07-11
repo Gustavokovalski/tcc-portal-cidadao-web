d7d6f3f00f0880a33f991e1beb1a54cdd4c424c2
Samuel <samuel.t.almeida@gmail.com>
2021-07-11 16:53:59 -0300

    adicionar mapa

--- portal-cidadao-web/src/app/modules/home/home.module.ts
+++ portal-cidadao-web/src/app/modules/home/home.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './page/home.component';
 
 import { HomeRoutingModule } from './home.routing';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
-  imports: [HomeRoutingModule],
+  imports: [
+    HomeRoutingModule, 
+    GoogleMapsModule
+  ],
   exports: [],
   providers: []
 })
