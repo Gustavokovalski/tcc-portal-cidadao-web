3aae0f217afd077b6b806360fb520b36560d0d75
Samuel T Almeida <samuel.almeida@capgemini.com>
2021-07-12 00:04:09 -0300

    melhorando posicao do mapa

--- portal-cidadao-web/src/app/modules/home/page/home.component.ts
+++ portal-cidadao-web/src/app/modules/home/page/home.component.ts
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  /*private zoom = 12
+  private center: google.maps.LatLngLiteral
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  }*/
+
+  public options: google.maps.MapOptions = {
+    center: {lat: -25.4372, lng: -49.2700},
+    zoom: 15
+  };
 
   constructor(
 
